594331bf40d7c4658b813157e3bf438c4d0f29d5
aakritibagri <121954665+aakritibagri@users.noreply.github.com>
2025-11-03 12:27:39 +0530

    Enhance MovieList to support favorites functionality

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-const MovieList = ({ movies, onMovieClick }) => {
-  if (!movies || movies.length === 0) return <p>No movies found</p>;
+const MovieList = ({ movies, onMovieClick, favorites = [], addFavorite, removeFavorite }) => {
+  if (!movies || movies.length === 0) {
+    return <p>No movies found</p>;
+  }
 
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieCard key={movie.imdbID} movie={movie} onClick={onMovieClick} />
+        <MovieCard
+          key={movie.imdbID}
+          movie={movie}
+          onClick={onMovieClick}
+          favorites={favorites}
+          addFavorite={addFavorite}
+          removeFavorite={removeFavorite}
+        />
       ))}
     </div>
   );
